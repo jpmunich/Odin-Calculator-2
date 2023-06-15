56c17e809e21f64a51ca81778820becd9bf4802d
jpmunich <parkercoyle8@gmail.com>
2023-06-15 06:22:16 -0400

    Added functionality to all four operations
    A textbox for the previous operand was created to store data. An equals button was added so operations may be performed. Lastly methods for each operation were made, and each are called based upon the user's selected operation when the equals button is pressed.

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const numberButtons = Array.from(document.getElementsByClassName("number-button"
 const operationButtons = Array.from(document.getElementsByClassName("operation-button"));
 
 const calcTextBottom = document.getElementById("calc-top-text-bottom");
+const calcTextTop = document.getElementById("calc-top-text-top");
 const deleteButton = document.getElementById("delete-button");
 const clearButton = document.getElementById("clear-button");
+const equalsButton = document.getElementById("equals-button");
 
 let selectedOperation = "none";
 
@@ -26,13 +28,53 @@ for (let i = 0; i < operationButtons.length; i++) {
     operationButtons[i].addEventListener("click", () => {
         if (operationButtons[i].innerText == "+") {
             selectedOperation = "addition";
+            calcTextTop.innerText = calcTextBottom.innerText;
+            calcTextBottom.innerText = "";
         } else if (operationButtons[i].innerText == "-") {
             selectedOperation = "subtraction";
+            calcTextTop.innerText = calcTextBottom.innerText;
+            calcTextBottom.innerText = "";
         } else if (operationButtons[i].innerText == "*") {
             selectedOperation = "multiplication";
+            calcTextTop.innerText = calcTextBottom.innerText;
+            calcTextBottom.innerText = "";
         } else if (operationButtons[i].innerText == "/") {
             selectedOperation = "division";
+            calcTextTop.innerText = calcTextBottom.innerText;
+            calcTextBottom.innerText = "";
         }
     })
 }
 
+function add(a, b) {
+    calcTextBottom.innerText = (parseFloat(a) + parseFloat(b));
+    calcTextTop.innerText = "";
+}
+
+function subtract(a, b) {
+    calcTextBottom.innerText = (parseFloat(a) - parseFloat(b));
+    calcTextTop.innerText = "";
+}
+function multiply(a, b) {
+    calcTextBottom.innerText = (parseFloat(a) * parseFloat(b));
+    calcTextTop.innerText = "";
+}
+
+function divide(a, b) {
+    calcTextBottom.innerText = (parseFloat(a) / parseFloat(b));
+    calcTextTop.innerText = "";
+}
+
+equalsButton.addEventListener("click", () => {
+    if (selectedOperation == "addition") {
+        add(parseFloat(calcTextTop.innerText), parseFloat(calcTextBottom.innerText));
+    } else if (selectedOperation == "subtraction") {
+        subtract(parseFloat(calcTextTop.innerText), parseFloat(calcTextBottom.innerText));
+    } else if (selectedOperation == "multiplication") {
+        multiply(parseFloat(calcTextTop.innerText), parseFloat(calcTextBottom.innerText));
+    } else if (selectedOperation == "division") {
+        divide(parseFloat(calcTextTop.innerText), parseFloat(calcTextBottom.innerText));
+    } else {
+    
+    }
+})
