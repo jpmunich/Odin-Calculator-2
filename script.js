f5c561a735dc2ec35cf5ff29e738cb85c8ef3d3e
jpmunich <parkercoyle8@gmail.com>
2023-06-14 22:54:33 -0400

    CLEAR removes input + operation button function
    Pressing the clear button will remove all existing input in the calculator.
    Added a string value that determines what operation the calculator should perform. Pressing each operation button changes the string.

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const operationButtons = Array.from(document.getElementsByClassName("operation-b
 
 const calcTextBottom = document.getElementById("calc-top-text-bottom");
 const deleteButton = document.getElementById("delete-button");
+const clearButton = document.getElementById("clear-button");
+
+let selectedOperation = "none";
 
 for (let i = 0; i < numberButtons.length; i++) {
     numberButtons[i].addEventListener("click", () => {
@@ -13,4 +16,23 @@ for (let i = 0; i < numberButtons.length; i++) {
 deleteButton.addEventListener("click", () => {
     calcTextBottom.innerText = calcTextBottom.innerText.substring(0, calcTextBottom.innerText.length - 1);
 })
+
+clearButton.addEventListener("click", () => {
+    console.log(selectedOperation);
+    calcTextBottom.innerText = "";
+})
+
+for (let i = 0; i < operationButtons.length; i++) {
+    operationButtons[i].addEventListener("click", () => {
+        if (operationButtons[i].innerText == "+") {
+            selectedOperation = "addition";
+        } else if (operationButtons[i].innerText == "-") {
+            selectedOperation = "subtraction";
+        } else if (operationButtons[i].innerText == "*") {
+            selectedOperation = "multiplication";
+        } else if (operationButtons[i].innerText == "/") {
+            selectedOperation = "division";
+        }
+    })
+}
 
